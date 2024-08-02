5a45306f5dbfebe5dc0312bab795b05a1f0b3116
KASHYAPCT <kashyapchandran38@gmail.com>
2024-08-02 11:20:44 +0530

    todo app task

---
+++ app1/views.py
@@ -0,0 +1,50 @@
+# views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoListCreateAPIView(APIView):
+    def get(self, request):
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TodoDetailAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Todo, pk=pk)
+
+    def get(self, request, pk):
+        todo = self.get_object(pk)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        todo = self.get_object(pk)
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        todo = self.get_object(pk)
+        serializer = TodoSerializer(todo, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        todo = self.get_object(pk)
+        todo.delete()
+        return Response({"message":"deleted successfuly....!"},status=status.HTTP_200_OK)
